2aaadc6d35b072965a407c25220ccf81072f45f5
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-06 10:21:33 +0100

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,22 +1,22 @@
 
 public class Solution {
 
-    public String freqAlphabets(String s) {
-        final int size = s.length();
+    public String freqAlphabets(String input) {
+        final int size = input.length();
         final int ascii_small_case_a = 97;
         final int ascii_zero = 48;
-        StringBuilder result = new StringBuilder();
+        StringBuilder decryptedInput = new StringBuilder();
 
         for (int i = size - 1; i >= 0; i--) {
-            if (s.charAt(i) == '#') {
-                int codePoint = 10 * (s.charAt(i - 2) - ascii_zero) + (s.charAt(i - 1) - ascii_zero);
-                result.append((char) (ascii_small_case_a + codePoint - 1));
+            if (input.charAt(i) == '#') {
+                int codePoint = 10 * (input.charAt(i - 2) - ascii_zero) + (input.charAt(i - 1) - ascii_zero);
+                decryptedInput.append((char) (ascii_small_case_a + codePoint - 1));
                 i -= 2;
             } else {
-                int codePoint = s.charAt(i) - ascii_zero;
-                result.append((char) (ascii_small_case_a + codePoint - 1));
+                int codePoint = input.charAt(i) - ascii_zero;
+                decryptedInput.append((char) (ascii_small_case_a + codePoint - 1));
             }
         }
-        return result.reverse().toString();
+        return decryptedInput.reverse().toString();
     }
 }
